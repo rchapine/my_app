124c7af4e3973042e4f3447aae59f1a0595af9a2
Rafael Chapine <rchapine@gmail.com>
2021-02-01 15:46:28 -0500

    added navbar, post,home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <section className="relative flex justify-center min-hight-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-black-100 font-bold cursive leading-snug home-name">
+          I`m Let`s Design
+        </h1>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/components/SinglePost.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SinglePost() {
+  return <h1>SingelPost</h1>;
+}
+
+export default SinglePost;
